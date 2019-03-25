0a8563a2ca5f1a2d590044df44b722330767d615
Mate Toth-Csak <tothcsakmate@gmail.com>
2019-03-25 14:42:20 +0100

    LoginController functionality implemented + a requestbody class created

--- src/main/java/xyz/playtheia/controller/LogInController.java
+++ src/main/java/xyz/playtheia/controller/LogInController.java
@@ -1,16 +1,29 @@
 package xyz.playtheia.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
+import xyz.playtheia.controller.requestBodies.LogInBody;
+import xyz.playtheia.model.Player;
+import xyz.playtheia.service.PlayerService;
 
 
 @RestController
 public class LogInController {
 
+    private PlayerService playerService;
 
-    @GetMapping("/")
-    public String greetUser() {
-        return "Welcome to Theia Online";
+    @Autowired
+    public LogInController(PlayerService playerService) {
+        this.playerService = playerService;
     }
+
+    @PostMapping("/login")
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+    public Player LogIn(@RequestBody LogInBody logInBody) {
+        return playerService.tryLogIn(logInBody.getEmail(), logInBody.getPassword());
+    }
+
+
 }
